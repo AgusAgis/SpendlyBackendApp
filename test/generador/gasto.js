4ef14f99d1036e33077a4537a4d18ad1911c756b
Matias Tello <matiastello1@hotmail.com>
2025-11-26 18:41:33 -0300

    arreglo en el generador de gasto

--- test/generador/gasto.js
+++ test/generador/gasto.js
@@ -7,16 +7,23 @@ const categorias = [
   'Hogar',
   'Educación',
   'Salud'
-]
-const get = () => ({
-    "id": faker.number.int({ min: 10, max: 10000 }),
-    "nombre": faker.commerce.department(),
-    "categoria": faker.helpers.arrayElement(categorias),
-    "monto": faker.finance.amount(),
-    "moneda": "ARS",
-    "fecha": faker.date.anytime(),
-})
+];
+
+const get = () => {
+  const fechaRandom = faker.date.anytime().toISOString().split('T')[0]; 
+  // → "2025-11-01"
+
+  return {
+    nombre: faker.commerce.department(), 
+    categoria: faker.helpers.arrayElement(categorias),
+    monto: Number(faker.finance.amount({ min: 1000, max: 50000, dec: 0 })), 
+    moneda: "ARS", // para no exigir tipoConversion
+    tipoConversion: null,
+    fecha: fechaRandom
+    // imagen y archivo no los mandamos: son opcionales según Joi
+  };
+};
 
 module.exports = {
-    get
-}
+  get
+};
